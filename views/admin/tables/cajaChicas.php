6b59698cc0699659090a48bfbd0bc2df32693dad
helier <hdnymib@gmail.com>
2015-07-22 01:00:45 -0400

    add litle opcions

--- views/admin/tables/cajaChicas.php
+++ views/admin/tables/cajaChicas.php
@@ -7,59 +7,60 @@ use yii\helpers\Url;
 
 ?>
 <?php
-    $columns = [
-        [
-            'header'=>'Sucursal',
-            'attribute'=>'sucursal',
-            'filterType'=>GridView::FILTER_SELECT2,
-            'filter'=>ArrayHelper::map(Sucursal::find()->all(),'idSucursal','nombre'),
-            'filterWidgetOptions'=>[
-                'pluginOptions'=>['allowClear'=>true],
-            ],
-            'filterInputOptions'=>['placeholder'=>'Seleccionar'],
-            'format'=>'raw',
-            'value'=>function($model){
-                return $model->fkIdCajaOrigen->fkIdSucursal->nombre;
-            },
+$columns = [
+    [
+        'header'=>'Sucursal',
+        'attribute'=>'sucursal',
+        'filterType'=>GridView::FILTER_SELECT2,
+        'filter'=>ArrayHelper::map(Sucursal::find()->all(),'idSucursal','nombre'),
+        'filterWidgetOptions'=>[
+            'pluginOptions'=>['allowClear'=>true],
         ],
-        [
-            'header'=>'Usuario',
-            'attribute'=>function($model)
-            {
-                return $model->fkIdUser->username;
-            }
-        ],
-        [
-            'header'=>'Nombre',
-            'attribute'=>'nombreUsuario',
-            'value'=>function($model)
-            {
-                return $model->fkIdUser->nombre." ".$model->fkIdUser->apellido;
-            }
-        ],
-        [
-            'header'=>'Monto',
-            'attribute'=>'monto',
-        ],
-        [
-            'header'=>'Detalle',
-            'attribute'=>'observaciones',
-        ],
-        [
-            'header'=>'Fecha',
-            'attribute'=>'time',
-        ],
-        [
-            'class' => 'yii\grid\ActionColumn',
-            'template'=>'{update}',
-            'buttons'=>[
-                'update'=>function($url,$model){
-                    $options = array_merge([
-                                               //'class'=>'btn btn-success',
-                                               'data-original-title'=>'Modificar',
-                                               'data-toggle'=>'tooltip',
-                                               'title'=>'',
-                                               'onclick'             => "
+        'filterInputOptions'=>['placeholder'=>'Seleccionar'],
+        'format'=>'raw',
+        'value'=>function($model){
+            return $model->fkIdCajaOrigen->fkIdSucursal->nombre;
+        },
+    ],
+    [
+        'header'=>'Usuario',
+        'attribute'=>function($model)
+        {
+            return $model->fkIdUser->username;
+        }
+    ],
+    [
+        'header'=>'Nombre',
+        'attribute'=>'nombreUsuario',
+        'value'=>function($model)
+        {
+            return $model->fkIdUser->nombre." ".$model->fkIdUser->apellido;
+        }
+    ],
+    [
+        'header'=>'Monto',
+        'attribute'=>'monto',
+        'pageSummary'=>true,
+    ],
+    [
+        'header'=>'Detalle',
+        'attribute'=>'observaciones',
+    ],
+    [
+        'header'=>'Fecha',
+        'attribute'=>'time',
+    ],
+    [
+        'class'=>'kartik\grid\ActionColumn',
+        'template'=>'{update}',
+        'buttons'=>[
+            'update'=>function($url,$model){
+                $options = array_merge([
+                    //'class'=>'btn btn-success',
+                    'data-original-title'=>'Modificar',
+                    'data-toggle'=>'tooltip',
+                    'title'=>'',
+                    'onclick'             => "
                                                         $.ajax({
                                                             type     :'POST',
                                                             cache    : false,
@@ -72,24 +73,25 @@ use yii\helpers\Url;
                                                                 }
                                                             }
                                                         });return false;"
-                                           ]);
-                        if(!empty($model->fechaCierre))
-                            return "";
-                    return Html::a('<span class="glyphicon glyphicon-pencil"></span>', "#", $options);
-                },
-            ]
-        ],
-    ];
-    echo GridView::widget([
-                              'dataProvider'=> $cajasChicas,
-                              'filterModel' => $search,
-                              'columns' => $columns,
-                              'toolbar' =>  [
-                                  [
-                                      'content'=>
-                                          Html::a('Nueva Transaccion', "#", [
-                                              'class'=>'btn btn-default',
-                                              'onclick'  => "
+                ]);
+                if(!empty($model->fechaCierre))
+                    return "";
+                return Html::a('<span class="glyphicon glyphicon-pencil"></span>', "#", $options);
+            },
+        ]
+    ],
+];
+echo GridView::widget([
+    'dataProvider'=> $cajasChicas,
+    'filterModel' => $search,
+    'columns' => $columns,
+    'showPageSummary' => true,
+    'toolbar' =>  [
+        [
+            'content'=>
+                Html::a('Nueva Transaccion', "#", [
+                    'class'=>'btn btn-default',
+                    'onclick'  => "
                     $.ajax({
                         type    :'POST',
                         cache   : false,
@@ -102,43 +104,44 @@ use yii\helpers\Url;
                             }
                         }
                     });return false;"
-                                          ]),
-                                      'options' => ['class' => 'btn-group']
-                                  ],
-                                  '{export}',
-                                  '{toggleData}',
-                              ],
-                              // set export properties
-                              'export' => [
-                                  'fontAwesome' => true,
-                                  'target'=>GridView::TARGET_BLANK,
-                              ],
-                              'responsive'=>true,
-                              'hover'=>true,
-                              'bordered'=>false,
-                              'panel' => [
-                                  'type' => GridView::TYPE_DEFAULT,
-                                  'heading' => 'Caja Chica',
-                              ],
-                              'exportConfig' => [
-                                  GridView::EXCEL => [
-                                      'label' => 'Excel',
-                                      'filename' => 'Reporte Caja Chica',
-                                      'alertMsg' => 'El EXCEL se generara para la descarga.',
-                                      'showPageSummary' => true,
-                                  ],
-                                  GridView::PDF => [
-                                      'label' => 'PDF',
-                                      'filename' => 'Reporte Caja Chica',
-                                      'alertMsg' => 'El PDF se generara para la descarga.',
-                                      'config' => [
-                                          'format' => 'Letter-L',
-                                          'marginTop' => 5,
-                                          'marginBottom' => 5,
-                                          'marginLeft' => 5,
-                                          'marginRight' => 5,
-                                      ]
-                                  ],
-                              ],
-                          ]);
+                ]),
+            'options' => ['class' => 'btn-group']
+        ],
+        '{export}',
+        '{toggleData}',
+    ],
+    // set export properties
+    'export' => [
+        'fontAwesome' => true,
+        'target'=>GridView::TARGET_BLANK,
+    ],
+    'responsive'=>true,
+    'hover'=>true,
+    'bordered'=>false,
+    'panel' => [
+        'type' => GridView::TYPE_DEFAULT,
+        'heading' => 'Caja Chica',
+        'footer'=>false,
+    ],
+    'exportConfig' => [
+        GridView::EXCEL => [
+            'label' => 'Excel',
+            'filename' => 'Reporte Caja Chica',
+            'alertMsg' => 'El EXCEL se generara para la descarga.',
+            'showPageSummary' => true,
+        ],
+        GridView::PDF => [
+            'label' => 'PDF',
+            'filename' => 'Reporte Caja Chica',
+            'alertMsg' => 'El PDF se generara para la descarga.',
+            'config' => [
+                'format' => 'Letter-L',
+                'marginTop' => 5,
+                'marginBottom' => 5,
+                'marginLeft' => 5,
+                'marginRight' => 5,
+            ]
+        ],
+    ],
+]);
 ?>
--- views/admin/tables/recibos.php
+++ views/admin/tables/recibos.php
@@ -7,67 +7,68 @@ use yii\helpers\Url;
 
 ?>
 <?php
-    $columns = [
-        [
-            'header'=>'Sucursal',
-            'attribute'=>'fk_idSucursal',
-            'filterType'=>GridView::FILTER_SELECT2,
-            'filter'=>ArrayHelper::map(Sucursal::find()->all(),'idSucursal','nombre'),
-            'filterWidgetOptions'=>[
-                'pluginOptions'=>['allowClear'=>true],
-            ],
-            'filterInputOptions'=>['placeholder'=>'Seleccionar'],
-            'format'=>'raw',
-            'value'=>function($model){
-                return $model->fkIdSucursal->nombre;
-            },
+$columns = [
+    [
+        'header'=>'Sucursal',
+        'attribute'=>'fk_idSucursal',
+        'filterType'=>GridView::FILTER_SELECT2,
+        'filter'=>ArrayHelper::map(Sucursal::find()->all(),'idSucursal','nombre'),
+        'filterWidgetOptions'=>[
+            'pluginOptions'=>['allowClear'=>true],
         ],
-        [
-            'header'=>'Tipo',
-            'filterType'=>GridView::FILTER_SELECT2,
-            'filter'=>["Egreso","Ingreso"],
-            'filterWidgetOptions'=>[
-                'pluginOptions'=>['allowClear'=>true],
-            ],
-            'filterInputOptions'=>['placeholder'=>'Seleccionar'],
-            'format'=>'raw',
-            'value'=>function($model) {
-                return (($model->tipoRecibo)?"Ingreso":"Egreso");
-            },
-            'attribute'=>'tipoRecibo',
+        'filterInputOptions'=>['placeholder'=>'Seleccionar'],
+        'format'=>'raw',
+        'value'=>function($model){
+            return $model->fkIdSucursal->nombre;
+        },
+    ],
+    [
+        'header'=>'Tipo',
+        'filterType'=>GridView::FILTER_SELECT2,
+        'filter'=>["Egreso","Ingreso"],
+        'filterWidgetOptions'=>[
+            'pluginOptions'=>['allowClear'=>true],
         ],
+        'filterInputOptions'=>['placeholder'=>'Seleccionar'],
+        'format'=>'raw',
+        'value'=>function($model) {
+            return (($model->tipoRecibo)?"Ingreso":"Egreso");
+        },
+        'attribute'=>'tipoRecibo',
+    ],
 
-        /*[
-            'header'=>'Codigo',
-            'attribute'=>'codigo',
-        ],*/
-        [
-            'header'=>'Nombre',
-            'attribute'=>'nombre',
-        ],
-        [
-            'header'=>'Monto',
-            'attribute'=>'monto',
-        ],
-        [
-            'header'=>'Detalle',
-            'attribute'=>'detalle',
-        ],
-        [
-            'header'=>'Fecha',
-            'attribute'=>'fechaRegistro',
-        ],
-        [
-            'class' => 'yii\grid\ActionColumn',
-            'template'=>'{update} {print}',
-            'buttons'=>[
-                'update'=>function($url,$model) {
-                    $options = array_merge([
-                                               //'class'=>'btn btn-success',
-                                               'data-original-title' => 'Modificar',
-                                               'data-toggle'         => 'tooltip',
-                                               'title'               => '',
-                                               'onclick'             => "
+    /*[
+        'header'=>'Codigo',
+        'attribute'=>'codigo',
+    ],*/
+    [
+        'header'=>'Nombre',
+        'attribute'=>'nombre',
+    ],
+    [
+        'header'=>'Monto',
+        'attribute'=>'monto',
+        'pageSummary'=>true,
+    ],
+    [
+        'header'=>'Detalle',
+        'attribute'=>'detalle',
+    ],
+    [
+        'header'=>'Fecha',
+        'attribute'=>'fechaRegistro',
+    ],
+    [
+        'class'=>'kartik\grid\ActionColumn',
+        'template'=>'{update} {print}',
+        'buttons'=>[
+            'update'=>function($url,$model) {
+                $options = array_merge([
+                    //'class'=>'btn btn-success',
+                    'data-original-title' => 'Modificar',
+                    'data-toggle'         => 'tooltip',
+                    'title'               => '',
+                    'onclick'             => "
                                                         $.ajax({
                                                             type     :'POST',
                                                             cache    : false,
@@ -80,37 +81,38 @@ use yii\helpers\Url;
                                                                 }
                                                             }
                                                         });return false;"
-                                           ]);
-                    $url     = "#";
-                    if(!empty($model->fkIdMovimientoCaja))
-                        if(!empty($model->fkIdMovimientoCaja->fechaCierre))
-                            return "";
-                    return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, $options);
-                },
-                'print'=>function($url,$model){
-                    $options = array_merge([
-                                               //'class'=>'btn btn-success',
-                                               'data-original-title'=>'Imprimir',
-                                               'data-toggle'=>'tooltip',
-                                               'title'=>''
-                                           ]);
-                    $url = Url::to(['venta/print','op'=>'recibo','id'=>$model->idRecibo]);
-                    return Html::a('<span class="glyphicon glyphicon-print"></span>', $url, $options);
-                },
-            ]
-        ],
-    ];
+                ]);
+                $url     = "#";
+                if(!empty($model->fkIdMovimientoCaja))
+                    if(!empty($model->fkIdMovimientoCaja->fechaCierre))
+                        return "";
+                return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, $options);
+            },
+            'print'=>function($url,$model){
+                $options = array_merge([
+                    //'class'=>'btn btn-success',
+                    'data-original-title'=>'Imprimir',
+                    'data-toggle'=>'tooltip',
+                    'title'=>''
+                ]);
+                $url = Url::to(['venta/print','op'=>'recibo','id'=>$model->idRecibo]);
+                return Html::a('<span class="glyphicon glyphicon-print"></span>', $url, $options);
+            },
+        ]
+    ],
+];
 
-    echo GridView::widget([
-                              'dataProvider'=> $recibos,
-                              'filterModel' => $search,
-                              'columns' => $columns,
-                              'toolbar' =>  [
-                                  [
-                                      'content'=>
-                                          Html::a('Recibo Ingreso', "#", [
-                                              'class'=>'btn btn-default',
-                                              'onclick'  => "
+echo GridView::widget([
+    'dataProvider'=> $recibos,
+    'filterModel' => $search,
+    'columns' => $columns,
+    'showPageSummary' => true,
+    'toolbar' =>  [
+        [
+            'content'=>
+                Html::a('Recibo Ingreso', "#", [
+                    'class'=>'btn btn-default',
+                    'onclick'  => "
                     $.ajax({
                         type    :'POST',
                         cache   : false,
@@ -123,11 +125,11 @@ use yii\helpers\Url;
                             }
                         }
                     });return false;"
-                                          ])
-                                          ." ".
-                                          Html::a('Recibo Egreso', "#", [
-                                              'class'=>'btn btn-default',
-                                              'onclick'  => "
+                ])
+                ." ".
+                Html::a('Recibo Egreso', "#", [
+                    'class'=>'btn btn-default',
+                    'onclick'  => "
                     $.ajax({
                         type    :'POST',
                         cache   : false,
@@ -140,43 +142,44 @@ use yii\helpers\Url;
                             }
                         }
                     });return false;"
-                                          ]),
-                                      'options' => ['class' => 'btn-group']
-                                  ],
-                                  '{export}',
-                                  '{toggleData}',
-                              ],
-                              // set export properties
-                              'export' => [
-                                  'fontAwesome' => true,
-                                  'target'=>GridView::TARGET_BLANK,
-                              ],
-                              'responsive'=>true,
-                              'hover'=>true,
-                              'bordered'=>false,
-                              'panel' => [
-                                  'type' => GridView::TYPE_DEFAULT,
-                                  'heading' => 'Recibos',
-                              ],
-                              'exportConfig' => [
-                                  GridView::EXCEL => [
-                                      'label' => 'Excel',
-                                      'filename' => 'Reporte Recibos',
-                                      'alertMsg' => 'El EXCEL se generara para la descarga.',
-                                      'showPageSummary' => true,
-                                  ],
-                                  GridView::PDF => [
-                                      'label' => 'PDF',
-                                      'filename' => 'Reporte Recibos',
-                                      'alertMsg' => 'El PDF se generara para la descarga.',
-                                      'config' => [
-                                          'format' => 'Letter-L',
-                                          'marginTop' => 5,
-                                          'marginBottom' => 5,
-                                          'marginLeft' => 5,
-                                          'marginRight' => 5,
-                                      ]
-                                  ],
-                              ],
-                          ]);
+                ]),
+            'options' => ['class' => 'btn-group']
+        ],
+        '{export}',
+        '{toggleData}',
+    ],
+    // set export properties
+    'export' => [
+        'fontAwesome' => true,
+        'target'=>GridView::TARGET_BLANK,
+    ],
+    'responsive'=>true,
+    'hover'=>true,
+    'bordered'=>false,
+    'panel' => [
+        'type' => GridView::TYPE_DEFAULT,
+        'heading' => 'Recibos',
+        'footer'=>false,
+    ],
+    'exportConfig' => [
+        GridView::EXCEL => [
+            'label' => 'Excel',
+            'filename' => 'Reporte Recibos',
+            'alertMsg' => 'El EXCEL se generara para la descarga.',
+            'showPageSummary' => true,
+        ],
+        GridView::PDF => [
+            'label' => 'PDF',
+            'filename' => 'Reporte Recibos',
+            'alertMsg' => 'El PDF se generara para la descarga.',
+            'config' => [
+                'format' => 'Letter-L',
+                'marginTop' => 5,
+                'marginBottom' => 5,
+                'marginLeft' => 5,
+                'marginRight' => 5,
+            ]
+        ],
+    ],
+]);
 ?>
